1baa3ce4d5aecbeafceb82f020314af8f35bc1fd
Arnaud Rebts <arnaud.rebts@gmail.com>
2017-02-03 19:22:22 +0100

    Set system preference programmaticaly

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Ask for the administrator password for the duration of this script
+sudo -v
+
 # Install Homebrew
 if ! [ -x "$(command -v brew)" ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -45,3 +48,21 @@ fish -c 'fundle install ; and exit'
 
 # Sets default iTerm2 profile
 defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "A879712F-6297-42E3-BFFD-9A5664D31470"
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: 3 fingers to swipe between pages, 4 to swipe between full screen apps
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Show IP address, hostname, OS version when clicking the clock in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
