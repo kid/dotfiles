a2fb24a2dbc1fe03e36b38882afc3c5d4ce58998
Arnaud Rebts <arnaud.rebts@gmail.com>
2017-02-06 15:28:22 +0100

    Disable diming of split panes in iTerm2

--- install.sh
+++ install.sh
@@ -72,6 +72,8 @@ fish -c 'fundle install'
 
 # Sets default iTerm2 profile
 defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "A879712F-6297-42E3-BFFD-9A5664D31470"
+# Disable diming inactive split panes
+defaults write com.googlecode.iterm2 "DimInactiveSplitPanes" -bool false
 
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -bool true
@@ -79,7 +81,6 @@ defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -bool true
 # Trackpad: 3 fingers to swipe between pages, 4 to swipe between full screen apps
 defaults write com.apple.driver.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 1
 defaults write com.apple.driver.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
-
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
