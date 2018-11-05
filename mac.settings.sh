2620f4e523c9eec397279c4978cea135dfd3d85c
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-05 10:35:07 +0100

    Adds a few settings for MacOS
    - [x] Allow key repeat for VS Code
    - [x] Disable mouse acceleration

--- mac.settings.sh
+++ mac.settings.sh
@@ -18,3 +18,9 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Allow key repeat for VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# Disable mouse acceleration
+defaults write -g com.apple.mouse.scaling -1
