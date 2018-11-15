b57fc248c4745c92def3391ac6faafcbbbb763cc
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-15 20:16:02 +0100

    Cleanup of fish config

--- stows/fish/.config/fish/config.fish
+++ stows/fish/.config/fish/config.fish
@@ -1,11 +1,12 @@
 # Install fundle if not available
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+set -U FZF_LEGACY_KEYBINDINGS 0
+
 # fundle plugins
-fundle plugin 'edc/bass'
 fundle plugin 'oh-my-fish/theme-bobthefish'
-fundle plugin 'fisherman/z'
-fundle plugin 'fisherman/getopts'
+fundle plugin 'jethrokuan/fzf'
+fundle plugin 'jethrokuan/z'
 fundle init
 
 # bobthefish configuration
@@ -13,38 +14,43 @@ set -g theme_color_scheme base16-dark
 set -g theme_nerd_fonts yes
 
 # Set missing locales
-set -x LC_ALL en_US.UTF-8
-set -x LANG en_US.UTF-8
+set LC_ALL en_US.UTF-8
+set LANG en_US.UTF-8
 
 # Set VIM as the default editor
-set -x EDITOR vim
+set EDITOR vim
 
 # Go setup
-set -x GOPATH "$HOME/Code/go"
-set -x PATH "$GOPATH/bin" $PATH
+set GOPATH "$HOME/Code/go"
+set PATH "$GOPATH/bin" $PATH
 
-set -x PATH "$HOME/.bin" "/usr/local/sbin" $PATH
+# Rbenv setup
+set PATH $HOME/.rbenv/bin $PATH
 
+set PATH "$HOME/.bin" "/usr/local/sbin" $PATH
+
+# For QMK Firmware development
+set -g fish_user_paths "/usr/local/opt/avr-gcc@7/bin" $fish_user_path
+
+alias cat='bat'
 alias get='wget -c --content-disposition'
 alias cp='cp -v'
 alias mv='mv -vi'
 alias rm='rm -vi'
+alias ll='exa -l'
+alias la='exa -la'
+alias lt='exa -lT'
+alias vim='nvim'
+alias git='hub'
 
 if test -e ~/.config/fish/exports.private.fish
   source ~/.config/fish/exports.private.fish
 end
 
-if test -e ~/.iterm2_shell_integration.fish
-  source ~/.iterm2_shell_integration.fish
-end
-
-if test -e ~/.asdf/asdf.fish
-  source ~/.asdf/asdf.fish
-end
-
-eval (direnv hook fish)
-
 # Base16 Shell
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-eighties.sh
+  set BASE16_SHELL "$HOME/.config/base16-shell/"
+  source "$BASE16_SHELL/profile_helper.fish"
 end
+
+set fish_key_bindings fish_user_key_bindings
