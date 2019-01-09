debfa68e127d491ad7266f1db8c4ef72638d954b
Arnaud Rebts <arnaud.rebts@gmail.com>
2019-01-09 20:03:16 +0100

    Adds zplugin, fzf and update installer for FreeBSD support

--- install.sh
+++ install.sh
@@ -1,27 +1,41 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
+os=$(uname -s)
+case $os in
+  "Darwin"*) 
+    # Allow apps from unindentifeid developers
+    sudo spctl --master-disable
+
+    if ! [ -x "$(command -v brew)" ]; then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+      brew update
+    fi
+
+    brew analytics off
+
+    # Install all our dependencies with bundle (See Brewfile)
+    brew tap homebrew/bundle
+    brew bundle
+    ;;
+  "FreeBSD"*)
+    sudo pkg install \
+      bash \
+      neovim \
+      stow \
+      tmux \
+      zsh
+    ;;
+  *)
+    echo "ERROR: unsupported OS: $os"
+    exit 1
+    ;;
+esac
+
 mkdir -p ~/.bin
 mkdir -p ~/Code/go/bin
-
-# Install Homebrew
-if [[ $OSTYPE =~ ^darwin ]]; then
-  # Allow apps from unindentifeid developers
-  sudo spctl --master-disable
-
-  if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    brew update
-  fi
-
-  brew analytics off
-
-  # Install all our dependencies with bundle (See Brewfile)
-  brew tap homebrew/bundle
-  brew bundle
-fi
 
 # Import GPG key from keybase
 # if ! gpg --list-keys D03B70DA66DF1DEA > /dev/null 2>&1; then
@@ -37,24 +51,14 @@ do
   stow --ignore='\.swp$' --dir=stows --target="$HOME" "${item#stows/}"
 done
 
-# Install fish plugins
-fish -c 'fundle install'
+"$HOME/.fzf/install.sh" --completion ----key-bindings --no-update-rc --no-bash --no-fish
 
-# Install vscode extensions
-code --install-extension EditorConfig.EditorConfig
-code --install-extension bsides.Theme-Base16-Eighties
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension christian-kohler.path-intellisense
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension lukehoban.go
-
-if [[ $OSTYPE =~ ^darwin ]]; then
+if [ "$os" = "Darwin" ]; then
   # docker-machine-driver-xhyve need root owner and uid
   # sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
   # sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 
   # Set macOS preferences
   # We will run this last because this will reload the shell
-  source mac.settings.sh
+  . mac.settings.sh
 fi
