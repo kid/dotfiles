05095e9721ff9727169a52bf9c790256fb3bc2b6
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-06-05 20:09:13 +0200

    feat: add gaps

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -574,3 +574,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+beautiful.useless_gap = 5
